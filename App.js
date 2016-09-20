67e05e36f6448af7c094d82e7fe8037c6a936c86
Michal Andros <michalandros@gmail.com>
2016-09-20 11:21:44 +0200

    some changes to basic files

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import  React from 'react';
 class App extends React.Component {
   render(){
-    return <div>Hello</div>
+    return <div>Hi Hello</div>
     }
   }
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   devServer: {
     inline: true,
-    port: 3333,
+    port: 3333
   },
   module: {
     loaders: [
